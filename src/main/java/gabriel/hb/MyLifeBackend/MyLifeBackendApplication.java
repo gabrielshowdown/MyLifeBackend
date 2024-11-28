1778c07fc26e6114c62e147398758c3ba42b147e
Gabriel Henrique de Bona <gabriell.hhenrique@hotmail.com>
2024-11-27 21:10:44 -0300

    Criação do Projeto

---
+++ src/main/java/gabriel/hb/MyLifeBackend/MyLifeBackendApplication.java
@@ -0,0 +1,13 @@
+package gabriel.hb.MyLifeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLifeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLifeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gabriel/hb/MyLifeBackend/MyLifeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gabriel.hb.MyLifeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLifeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
