48b66988be7a739b5fcdff65d7d4b44e2cc89e67
Gabriel Henrique de Bona <gabriell.hhenrique@hotmail.com>
2024-12-04 11:52:50 -0300

    Criando classes de excecao

---
+++ src/main/java/gabriel/hb/MyLifeBackend/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,33 @@
+package gabriel.hb.MyLifeBackend.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import gabriel.hb.MyLifeBackend.services.exceptions.DatabaseException;
+import gabriel.hb.MyLifeBackend.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice // Intercepta as exceções que acontecem, para que esse objeto faça um possível tratamento.
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class) // Esse método 'resourceNotFound' vai interceptar qq exceção desse tipo 'ResourceNotFoundException' 
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class) // Esse método 'database' vai interceptar qq exceção desse tipo 'DatabaseException' 
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
---
+++ src/main/java/gabriel/hb/MyLifeBackend/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package gabriel.hb.MyLifeBackend.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Recurso nao encontrado. ID " + id);
+	}
+	
+}
